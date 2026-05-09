9cde5ecb736749a2cac6b7f8e80086ff8373f597
Diego Sotelo <diego.sotelo@tecsup.edu.pe>
2026-05-09 18:44:54 -0500

    add crud views for patients and doctors

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Paciente, Doctor
+from .serializers import PacienteSerializer, DoctorSerializer
 
-# Create your views here.
+
+class PacienteViewSet(viewsets.ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'diagnostico', 'doctor__nombre', 'doctor__especialidad']
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
