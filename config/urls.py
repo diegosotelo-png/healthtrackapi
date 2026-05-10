505d6fc19ea632f2b73f7a4f6fc97de7f8611d8d
Diego Sotelo <diego.sotelo@tecsup.edu.pe>
2026-05-09 19:18:02 -0500

    complete crud endpoints for patients and doctors

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('api/')),
     path('api/', include('pacientes.urls')),
 ]
